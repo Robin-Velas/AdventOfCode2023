31502040d26da3bf6d1d5fd19412df21c5b15788
rmarques <robin.marques@edu.univ-fcomte.fr>
2023-12-01 15:53:27 +0100

    enhanced the code, made it more understandable and maintainable. added to gitignore.

--- day1/main.c
+++ day1/main.c
@@ -8,118 +8,129 @@
 #define PART2 !PART1
 #define SAMPLE 0
 
-int extractIntFromString(char* str);
-int extractIntAndStringFromString(char* str);
+typedef struct
+{
+    char *m_name;
+    int m_value;
+} Number;
+
+Number g_numbers[9] = {
+    {"one", 1},
+    {"two", 2},
+    {"three", 3},
+    {"four", 4},
+    {"five", 5},
+    {"six", 6},
+    {"seven", 7},
+    {"eight", 8},
+    {"nine", 9}
+};
+
+int extractIntFromString(char *str);
+int extractIntAndStringFromString(char *str);
 int main(void)
 {
-    FILE *fp;
-    char str[MAXCHAR];
-    #if SAMPLE
-    char* filename = "sample.txt";
-    #else
-    char* filename = "input.txt";
-    #endif
+    FILE *l_fp;
+    char l_str[MAXCHAR];
+#if SAMPLE
+    char *l_filename = "sample.txt";
+#else
+    char *l_filename = "input.txt";
+#endif
 
-    fp = fopen(filename, "r");
-    if (fp == NULL) {
-        printf("Could not open file %s", filename);
+    l_fp = fopen(l_filename, "r");
+    if (l_fp == NULL)
+    {
+        printf("Could not open file %s", l_filename);
         return 1;
     }
 
-    int sum = 0;
-    while (fgets(str, MAXCHAR, fp) != NULL) {
-        #if PART1
-        int num = extractIntFromString(str);
-        #elif PART2
-        int num = extractIntAndStringFromString(str);
-        #endif
-        printf("%d\n", num);
-        if (num != -1) {
-            sum += num;
+    int l_sum = 0;
+    while (fgets(l_str, MAXCHAR, l_fp) != NULL)
+    {
+#if PART1
+        int l_num = extractIntFromString(str);
+#elif PART2
+        int l_num = extractIntAndStringFromString(l_str);
+#endif
+        printf("%d\n", l_num);
+        if (l_num != -1)
+        {
+            l_sum += l_num;
         }
     }
-    printf("%d\n", sum);
-    fclose(fp);
+    printf("%d\n", l_sum);
+    fclose(l_fp);
 
     return 0;
 }
 
-int extractIntFromString(char* str)
+int extractIntFromString(char *p_str)
 {
-    int first = -1;
-    int last = -1;
-    for (int i = 0; i < strlen(str); i++) {
-        if (str[i] >= '0' && str[i] <= '9') {
-            if (first == NOT_FOUND) {
-                first = str[i] - '0';
-            } else {
-                last = str[i] - '0';
+    int l_first = -1;
+    int l_last = -1;
+    for (int l_i = 0; l_i < strlen(p_str); l_i++)
+    {
+        if (p_str[l_i] >= '0' && p_str[l_i] <= '9')
+        {
+            if (l_first == NOT_FOUND)
+            {
+                l_first = p_str[l_i] - '0';
+                l_last = l_first;
+            }
+            else
+            {
+                l_last = p_str[l_i] - '0';
             }
         }
     }
-    if (last == -1 && first != -1) {
-        last = first;
-    } 
-    if (first != -1 && last != -1) {
-        return first * 10 + last;
+    if (l_first != NOT_FOUND)
+    {
+        return l_first * 10 + l_last;
     }
-    return -1;
+    return NOT_FOUND;
 }
 
-int extractIntAndStringFromString(char* str)
+int extractIntAndStringFromString(char *p_str)
 {
-    char one[3] = "one";
-    char two[3] = "two";
-    char three[5] = "three";
-    char four[4] = "four";
-    char five[4] = "five";
-    char six[3] = "six";
-    char seven[5] = "seven";
-    char eight[5] = "eight";
-    char nine[4] = "nine";
+    int l_first = -1;
+    int l_last = -1;
 
-    int first = -1;
-    int last = -1;
+    char *l_curr = NULL;
 
-    char* curr = NULL;
-
-    for (int i = 0; i < strlen(str); i++) {
+    for (int l_i = 0; l_i < strlen(p_str); l_i++)
+    {
         // we deal with numbers
-        if (str[i] >= '0' && str[i] <= '9') {
-            if (first == -1) {
-                first = str[i] - '0';
-            } else {
-                last = str[i] - '0';
+        if (p_str[l_i] >= '0' && p_str[l_i] <= '9')
+        {
+            if (l_first == -1)
+            {
+                l_first = p_str[l_i] - '0';
+                l_last = l_first;
             }
-        } else { // we deal with chars
-            curr = &str[i];
-            int* currVal = first == -1 ? &first : &last;
-            if (strncmp(curr, one, 3) == 0) {
-                *currVal = 1;
-            } else if (strncmp(curr, two, 3) == 0) {
-                *currVal = 2;
-            } else if (strncmp(curr, three, 5) == 0) {
-                *currVal = 3;
-            } else if (strncmp(curr, four, 4) == 0) {
-                *currVal = 4;
-            } else if (strncmp(curr, five, 4) == 0) {
-                *currVal = 5;
-            } else if (strncmp(curr, six, 3) == 0) {
-                *currVal = 6;
-            } else if (strncmp(curr, seven, 5) == 0) {
-                *currVal = 7;
-            } else if (strncmp(curr, eight, 5) == 0) {
-                *currVal = 8;
-            } else if (strncmp(curr, nine, 4) == 0) {
-                *currVal = 9;
+            else
+            {
+                l_last = p_str[l_i] - '0';
+            }
+        }
+        else
+        { // we deal with chars
+            l_curr = &p_str[l_i];
+            int *l_currVal = l_first == -1 ? &l_first : &l_last;
+
+            for (int j = 0; j < 9; j++)
+            {
+                if (strncmp(l_curr, g_numbers[j].m_name, strlen(g_numbers[j].m_name)) == 0)
+                {
+                    *l_currVal = g_numbers[j].m_value;
+                    break;
+                }
             }
         }
     }
-    if (last == -1 && first != -1) {
-        last = first;
+    if (l_first != NOT_FOUND)
+    {
+        return l_first * 10 + l_last;
     }
-    if (first != -1 && last != -1) {
-        return first * 10 + last;
-    }
-    return -1;
+    return NOT_FOUND;
 }
